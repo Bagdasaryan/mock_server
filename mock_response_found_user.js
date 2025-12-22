81b7f558d9c240aa5819830d22a5fa68c9e79b40
Bagdasaryan <55394878+Bagdasaryan@users.noreply.github.com>
2025-12-22 14:49:01 +0100

    Add mock response for found user

---
+++ mock_response_found_user.js
@@ -0,0 +1,12 @@
+{
+  "responseCode": "200",
+  "responseBody": {
+    "foundPatient": {
+      "id": "a1",
+      "name": "Patient 1",
+      "bloodPressure": "140/90",
+      "bloodSugar": "6.5",
+      "heartBeat": "80"
+    }
+  }
+}
