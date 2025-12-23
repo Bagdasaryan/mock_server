353c5f83a3e5f05506f81ea58d2c0cc3138b2fa5
Bagdasaryan <55394878+Bagdasaryan@users.noreply.github.com>
2025-12-23 16:27:17 +0100

    Add list of all patients with vitals data

---
+++ list_of_all_patients.js
@@ -0,0 +1,174 @@
+{
+  "responseCode": "200",
+  "responseBody": {
+    "patients": [
+      {
+        "id": "1",
+        "name": "Patient 1",
+        "vitals": [
+          { "bloodPressure": "120", "bloodSugar": "5.8", "heartBeats": "78", "dateTimeInMillis": "1735732800000" },
+          { "bloodPressure": "122", "bloodSugar": "6.0", "heartBeats": "80", "dateTimeInMillis": "1735754400000" },
+          { "bloodPressure": "118", "bloodSugar": "5.6", "heartBeats": "76", "dateTimeInMillis": "1735905600000" },
+          { "bloodPressure": "121", "bloodSugar": "5.9", "heartBeats": "79", "dateTimeInMillis": "1735992000000" }
+        ]
+      },
+      {
+        "id": "2",
+        "name": "Patient 2",
+        "vitals": [
+          { "bloodPressure": "130", "bloodSugar": "6.2", "heartBeats": "85", "dateTimeInMillis": "1736942400000" },
+          { "bloodPressure": "128", "bloodSugar": "6.0", "heartBeats": "83", "dateTimeInMillis": "1736964000000" },
+          { "bloodPressure": "129", "bloodSugar": "6.1", "heartBeats": "84", "dateTimeInMillis": "1737129600000" },
+          { "bloodPressure": "127", "bloodSugar": "5.9", "heartBeats": "82", "dateTimeInMillis": "1737216000000" },
+          { "bloodPressure": "131", "bloodSugar": "6.3", "heartBeats": "86", "dateTimeInMillis": "1737302400000" }
+        ]
+      },
+      {
+        "id": "3",
+        "name": "Patient 3",
+        "vitals": [
+          { "bloodPressure": "125", "bloodSugar": "5.7", "heartBeats": "79", "dateTimeInMillis": "1738368000000" },
+          { "bloodPressure": "126", "bloodSugar": "5.9", "heartBeats": "81", "dateTimeInMillis": "1738389600000" },
+          { "bloodPressure": "124", "bloodSugar": "5.6", "heartBeats": "78", "dateTimeInMillis": "1738454400000" },
+          { "bloodPressure": "123", "bloodSugar": "5.5", "heartBeats": "77", "dateTimeInMillis": "1738540800000" },
+          { "bloodPressure": "127", "bloodSugar": "6.0", "heartBeats": "82", "dateTimeInMillis": "1738627200000" },
+          { "bloodPressure": "126", "bloodSugar": "5.8", "heartBeats": "80", "dateTimeInMillis": "1738713600000" }
+        ]
+      },
+      {
+        "id": "4",
+        "name": "Patient 4",
+        "vitals": [
+          { "bloodPressure": "118", "bloodSugar": "5.4", "heartBeats": "74", "dateTimeInMillis": "1739539200000" },
+          { "bloodPressure": "119", "bloodSugar": "5.5", "heartBeats": "75", "dateTimeInMillis": "1739560800000" },
+          { "bloodPressure": "117", "bloodSugar": "5.3", "heartBeats": "73", "dateTimeInMillis": "1739625600000" },
+          { "bloodPressure": "120", "bloodSugar": "5.6", "heartBeats": "76", "dateTimeInMillis": "1739712000000" }
+        ]
+      },
+      {
+        "id": "5",
+        "name": "Patient 5",
+        "vitals": [
+          { "bloodPressure": "135", "bloodSugar": "6.7", "heartBeats": "90", "dateTimeInMillis": "1740787200000" },
+          { "bloodPressure": "134", "bloodSugar": "6.5", "heartBeats": "88", "dateTimeInMillis": "1740808800000" },
+          { "bloodPressure": "136", "bloodSugar": "6.8", "heartBeats": "91", "dateTimeInMillis": "1740873600000" },
+          { "bloodPressure": "133", "bloodSugar": "6.4", "heartBeats": "87", "dateTimeInMillis": "1740960000000" },
+          { "bloodPressure": "132", "bloodSugar": "6.3", "heartBeats": "86", "dateTimeInMillis": "1741046400000" }
+        ]
+      },
+
+      {
+        "id": "6",
+        "name": "Patient 6",
+        "vitals": [
+          { "bloodPressure": "121", "bloodSugar": "5.9", "heartBeats": "78", "dateTimeInMillis": "1741996800000" },
+          { "bloodPressure": "122", "bloodSugar": "6.0", "heartBeats": "79", "dateTimeInMillis": "1742018400000" },
+          { "bloodPressure": "120", "bloodSugar": "5.8", "heartBeats": "77", "dateTimeInMillis": "1742083200000" },
+          { "bloodPressure": "123", "bloodSugar": "6.1", "heartBeats": "80", "dateTimeInMillis": "1742169600000" }
+        ]
+      },
+
+      {
+        "id": "7",
+        "name": "Patient 7",
+        "vitals": [
+          { "bloodPressure": "140", "bloodSugar": "7.0", "heartBeats": "95", "dateTimeInMillis": "1743033600000" },
+          { "bloodPressure": "138", "bloodSugar": "6.8", "heartBeats": "93", "dateTimeInMillis": "1743055200000" },
+          { "bloodPressure": "139", "bloodSugar": "6.9", "heartBeats": "94", "dateTimeInMillis": "1743120000000" },
+          { "bloodPressure": "137", "bloodSugar": "6.7", "heartBeats": "92", "dateTimeInMillis": "1743206400000" },
+          { "bloodPressure": "136", "bloodSugar": "6.6", "heartBeats": "91", "dateTimeInMillis": "1743292800000" }
+        ]
+      },
+
+      {
+        "id": "8",
+        "name": "Patient 8",
+        "vitals": [
+          { "bloodPressure": "115", "bloodSugar": "5.2", "heartBeats": "72", "dateTimeInMillis": "1744329600000" },
+          { "bloodPressure": "116", "bloodSugar": "5.3", "heartBeats": "73", "dateTimeInMillis": "1744351200000" },
+          { "bloodPressure": "114", "bloodSugar": "5.1", "heartBeats": "71", "dateTimeInMillis": "1744416000000" },
+          { "bloodPressure": "117", "bloodSugar": "5.4", "heartBeats": "74", "dateTimeInMillis": "1744502400000" }
+        ]
+      },
+
+      {
+        "id": "9",
+        "name": "Patient 9",
+        "vitals": [
+          { "bloodPressure": "128", "bloodSugar": "6.1", "heartBeats": "84", "dateTimeInMillis": "1745539200000" },
+          { "bloodPressure": "129", "bloodSugar": "6.2", "heartBeats": "85", "dateTimeInMillis": "1745560800000" },
+          { "bloodPressure": "127", "bloodSugar": "6.0", "heartBeats": "83", "dateTimeInMillis": "1745625600000" },
+          { "bloodPressure": "130", "bloodSugar": "6.3", "heartBeats": "86", "dateTimeInMillis": "1745712000000" }
+        ]
+      },
+
+      {
+        "id": "10",
+        "name": "Patient 10",
+        "vitals": [
+          { "bloodPressure": "132", "bloodSugar": "6.4", "heartBeats": "87", "dateTimeInMillis": "1746835200000" },
+          { "bloodPressure": "131", "bloodSugar": "6.3", "heartBeats": "86", "dateTimeInMillis": "1746856800000" },
+          { "bloodPressure": "133", "bloodSugar": "6.5", "heartBeats": "88", "dateTimeInMillis": "1746921600000" },
+          { "bloodPressure": "130", "bloodSugar": "6.2", "heartBeats": "85", "dateTimeInMillis": "1747008000000" },
+          { "bloodPressure": "129", "bloodSugar": "6.1", "heartBeats": "84", "dateTimeInMillis": "1747094400000" }
+        ]
+      },
+
+      {
+        "id": "11",
+        "name": "Patient 11",
+        "vitals": [
+          { "bloodPressure": "118", "bloodSugar": "5.5", "heartBeats": "75", "dateTimeInMillis": "1748131200000" },
+          { "bloodPressure": "119", "bloodSugar": "5.6", "heartBeats": "76", "dateTimeInMillis": "1748152800000" },
+          { "bloodPressure": "117", "bloodSugar": "5.4", "heartBeats": "74", "dateTimeInMillis": "1748217600000" },
+          { "bloodPressure": "120", "bloodSugar": "5.7", "heartBeats": "77", "dateTimeInMillis": "1748304000000" }
+        ]
+      },
+
+      {
+        "id": "12",
+        "name": "Patient 12",
+        "vitals": [
+          { "bloodPressure": "145", "bloodSugar": "7.2", "heartBeats": "98", "dateTimeInMillis": "1749331200000" },
+          { "bloodPressure": "143", "bloodSugar": "7.0", "heartBeats": "96", "dateTimeInMillis": "1749352800000" },
+          { "bloodPressure": "144", "bloodSugar": "7.1", "heartBeats": "97", "dateTimeInMillis": "1749417600000" },
+          { "bloodPressure": "142", "bloodSugar": "6.9", "heartBeats": "95", "dateTimeInMillis": "1749504000000" },
+          { "bloodPressure": "141", "bloodSugar": "6.8", "heartBeats": "94", "dateTimeInMillis": "1749590400000" }
+        ]
+      },
+
+      {
+        "id": "13",
+        "name": "Patient 13",
+        "vitals": [
+          { "bloodPressure": "123", "bloodSugar": "5.8", "heartBeats": "79", "dateTimeInMillis": "1750636800000" },
+          { "bloodPressure": "124", "bloodSugar": "5.9", "heartBeats": "80", "dateTimeInMillis": "1750658400000" },
+          { "bloodPressure": "122", "bloodSugar": "5.7", "heartBeats": "78", "dateTimeInMillis": "1750723200000" },
+          { "bloodPressure": "125", "bloodSugar": "6.0", "heartBeats": "81", "dateTimeInMillis": "1750809600000" }
+        ]
+      },
+
+      {
+        "id": "14",
+        "name": "Patient 14",
+        "vitals": [
+          { "bloodPressure": "129", "bloodSugar": "6.2", "heartBeats": "84", "dateTimeInMillis": "1751846400000" },
+          { "bloodPressure": "130", "bloodSugar": "6.3", "heartBeats": "85", "dateTimeInMillis": "1751868000000" },
+          { "bloodPressure": "128", "bloodSugar": "6.1", "heartBeats": "83", "dateTimeInMillis": "1751932800000" },
+          { "bloodPressure": "131", "bloodSugar": "6.4", "heartBeats": "86", "dateTimeInMillis": "1752019200000" }
+        ]
+      },
+
+      {
+        "id": "15",
+        "name": "Patient 15",
+        "vitals": [
+          { "bloodPressure": "116", "bloodSugar": "5.3", "heartBeats": "73", "dateTimeInMillis": "1766102400000" },
+          { "bloodPressure": "117", "bloodSugar": "5.4", "heartBeats": "74", "dateTimeInMillis": "1766124000000" },
+          { "bloodPressure": "115", "bloodSugar": "5.2", "heartBeats": "72", "dateTimeInMillis": "1766188800000" },
+          { "bloodPressure": "118", "bloodSugar": "5.5", "heartBeats": "75", "dateTimeInMillis": "1766275200000" }
+        ]
+      }
+    ]
+  }
+}
